b1bf64a26bddf507693e16ac8f43ce574ba24fab
moe1129peterson <moe1129tsuzuki@gmail.com>
2021-10-31 23:12:36 -0600

    changes

--- DGM3760/Book Tracker App/frontend/src/templates.js
+++
@@ -1,7 +0,0 @@
-export const jobTemplate = (title, author_name) => `
-<div class="card">
-  <div class="card-body">
-  <h4 class="card-title">${title.title}:${author_name}</h4>
-  </div>
-</div>
-`;

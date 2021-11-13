707f4ed43a4cf8f154b53eaaf9ac02b96e8ef406
moe1129peterson <moe1129tsuzuki@gmail.com>
2021-11-12 18:56:15 -0700

    updates

---
+++ DGM3760/Book Tracker 2/server.js
@@ -0,0 +1,43 @@
+const express = require('express'); 
+const bodyParser = require ('body-parser'); 
+const mongoose = require ('mongoose'); 
+const {mongoUser,mongoPass} = require('./config')
+const cors = require ('cors'); 
+
+const uri = `mongodb+srv://${mongoUser}:${mongoPass}@cluster0.riadj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+//Routes 
+const postsRoutes = require ('./routes/api/posts');
+const app = express(); 
+
+//Body Parser Middleware 
+app.use (express.json()); 
+app.use(cors());
+
+//connect to MongoDB
+mongoose.connect (process.env.MONGO_URI,{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+})
+
+.then(() => console.log ('Connected MongoDB Sucessfully'))
+
+.catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send ('Hello from the other side')
+}); 
+
+//User routes
+
+app.use('/api/posts', postsRoutes); 
+const PORT = process.env.PORT || 5000; 
+app.listen (PORT, () => console.log (`Server runs at ${PORT}`))
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+app.use(express.static('public'))

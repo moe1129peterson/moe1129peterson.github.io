b781cf985738077063adfc45358ff659f94bf104
moe1129peterson <moe1129tsuzuki@gmail.com>
2021-12-08 20:03:59 -0700

    worked on 2740

---
+++ DGM2740/10_Swag_Shop/js/scripts.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+var x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
